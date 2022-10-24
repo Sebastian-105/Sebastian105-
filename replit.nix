915eda7f8f98645bba34fbdd20899653c4b647a8
Sebastian105 <83544451+Sebastian-105@users.noreply.github.com>
2022-10-24 23:00:43 +0000

    hi

--- replit.nix
+++ replit.nix
@@ -1,5 +1,8 @@
 { pkgs }: {
   deps = [
+    pkgs.sudo
+    pkgs.busybox-sandbox-sudo
+    pkgs.wallabag
     pkgs.busybox-sandbox-shell
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
